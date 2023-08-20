62b7f64b71100603f0a0c3daf1a8bbce6c0c13ff
shirow ozawa <striderkein@users.noreply.github.com>
2023-08-20 16:17:22 +0900

    feat:いろいろ実装
    - 人数入力時のバリデーション
    - clear console
    - details

--- main.rb
+++ main.rb
@@ -20,46 +20,128 @@ details = 'なし'
 
 process_end = false
 
+def clear_console
+  puts "\e[H\e[2J"
+end
+
+def is_not_number(str)
+	return str.match(/[^0-9]+/)
+end
+
+clear_console()
+
 while !process_end
 	puts "########################\nチケット料金を計算します\n########################"
 
-	# 人数を入力
 	# 大人
-	print '大人の人数を入力> '
-	adult_normal = gets.chomp
-	print 'チラシの枚数（大人）を入力> '
-	adult_special = gets.chomp
+	while true
+		print '大人の人数を入力> '
+		adult_normal = gets.chomp
+		if is_not_number(adult_normal) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+	end
+
+	clear_console()
+
+	while true
+		print 'チラシの枚数（大人）を入力> '
+		adult_special = gets.chomp
+		if is_not_number(adult_special) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+	end
+
+	clear_console()
 
 	# 子供
-	print '子供の人数を入力> '
-	child_normal = gets.chomp
-	print 'チラシの枚数（子供）を入力> '
-	child_special = gets.chomp
+	while true
+		print '子供の人数を入力> '
+		child_normal = gets.chomp
+		if is_not_number(child_normal) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+	end
+
+	clear_console()
+
+	while true
+		print 'チラシの枚数（子供）を入力> '
+		child_special = gets.chomp
+		if is_not_number(child_special) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+	end
+
+	clear_console()
 
 	# シニア
-	print 'シニアの人数を入力> '
-	senior_normal = gets.chomp
-	print 'チラシの枚数（シニア）を入力> '
-	senior_special = gets.chomp
+	while true
+		print 'シニアの人数を入力> '
+		senior_normal = gets.chomp
+		if is_not_number(senior_normal) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+	end
+
+	clear_console()
+
+	while true
+		print 'チラシの枚数（シニア）を入力> '
+		senior_special = gets.chomp
+		if is_not_number(senior_special) then
+			puts '数値を入力してください'
+			next
+		else
+			break
+		end
+  end
+
+	clear_console()
 
 	# TODO: 団体割引 → 10人以上で10%割引（子供は 0.5 人換算とする）
 	# TODO: 夕方料金 → 17時以降は 100円 引き
+	#   - 現在時刻を取得して変数nowに代入 → now = Time.now
 	# TODO: 休日料金 → 土日祝は 200円 増し
 	# TODO: 月水割引 → 月曜と水曜は 100円 引き
-	# TODO: 現在時刻を取得して、変数nowに代入 → now = Time.now
-
+	raw_fee_adult = adult_normal.to_i * fee_adult
+	raw_fee_child = child_normal.to_i * fee_child
+	raw_fee_senior = senior_normal.to_i * fee_senior
+	discount_adult_total = adult_special.to_i * DISCOUNT_ADULT
+	discount_child_total = child_special.to_i * DISCOUNT_CHILD
+	discount_senior_total = senior_special.to_i * DISCOUNT_SENIOR
 	# TODO: 全てのチケット料金を計算して、変数total_feeに代入
-	total_fee = adult_normal.to_i * fee_adult - adult_special.to_i * DISCOUNT_ADULT + child_normal.to_i * fee_child - child_special.to_i * DISCOUNT_CHILD + senior_normal.to_i * fee_senior - senior_special.to_i * DISCOUNT_SENIOR
+	total_fee =
+		raw_fee_adult - discount_adult_total +
+		raw_fee_child - discount_child_total +
+		raw_fee_senior - discount_senior_total
 	raw_fee = total_fee
 
+	details = "|        |   通常   |   割引   |\n+--------+----------+----------+\n|  大人  |  #{adult_normal} 名様 |  #{adult_special} 名様 |\n|  子供  |  #{child_normal} 名様 |  #{child_special} 名様 |\n| シニア |  #{senior_normal} 名様 |  #{senior_special} 名様 |\n+--------+----------+----------+"
 	# puts "大人:#{adult_normal}\n子供:#{child_normal}\nシニア:#{senior_normal}\n大人（特別）:#{adult_special}\n子供（特別）:#{child_special}\nシニア（特別）:#{senior_special}"
 	# TODO: impl
-	puts "販売合計金額:#{total_fee}\n金額変更前合計金額:#{raw_fee}\n金額変更明細:#{details}\n"
+	puts "販売合計金額:#{total_fee}\n金額変更前合計金額:#{raw_fee}\n金額変更明細:\n#{details}\n"
 
 	while true
 		print "処理を継続しますか？(yes | no)> "
 		answer = gets.chomp
 		if answer == 'yes' then
+			clear_console()
 			break
 		elsif answer == 'no' then
 			process_end = true
@@ -72,3 +154,4 @@ while !process_end
 end
 
 puts '終了します'
+clear_console()
