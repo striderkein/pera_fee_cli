7b3d3b8e592414ff0726de016d62b2924ea6a3fb
shirow ozawa <striderkein@users.noreply.github.com>
2023-08-20 20:21:17 +0900

    feat:団体割引以外の「その他割引・割増」を実装
    たぶん一通りできてると思う

--- main.rb
+++ main.rb
@@ -1,26 +1,7 @@
-# print 'hello (print)' # 改行なしで出力
-# puts 'hello(put)' # 改行ありで出力
-# p 'hello(p)' # デバッグ用出力（データ形式がわかる）
-
 # TODO: JSON ファイルを読み込む
-# 定数
-fee_adult = 1000
-fee_adult_sp = 600
-DISCOUNT_ADULT = fee_adult - fee_adult_sp
-fee_child = 500
-fee_child_sp = 400
-DISCOUNT_CHILD = fee_child - fee_child_sp
-fee_senior = 800
-fee_senior_sp = 500
-DISCOUNT_SENIOR = fee_senior - fee_senior_sp
-
-GROUP_DISCONT_RATE = 0.9
-
-total_fee = 0
-raw_fee = 0
-details = 'なし'
-
-process_end = false
+$base_fee_adult = 1000
+$base_fee_child = 500
+$base_fee_senior = 800
 
 def clear_console
   puts "\e[H\e[2J"
@@ -29,6 +10,71 @@ end
 def is_not_number(str)
 	return str.match(/[^0-9]+/)
 end
+
+def is_night()
+	return Time.now.hour >= 17
+end
+
+# 休日かどうかを判定する
+# 土日のみを休日とする
+# @return [Boolean] 休日かどうか
+def is_holiday()
+	return Time.now.wday == 0 || Time.now.wday == 6
+end
+
+# 実行時点の曜日が月曜日または水曜日であるかを判定する
+# @return [Boolean] 月曜日または水曜日か否か
+def is_mon_wed()
+	return Time.now.wday == 1 || Time.now.wday == 3
+end
+
+def decide_fee_type()
+  if is_holiday() then
+		return 'holiday'
+	elsif is_mon_wed() then
+		return 'mon_wed'
+	elsif is_night() then
+		return 'night'
+	else
+		return 'nodiscount'
+	end
+end
+
+def decide_fee(age_group)
+	discount_amount_mon_wed = 100
+	discount_amount_night = 100
+	increase_amount_holiday = 200
+	base_fee = age_group == 'adult' ? $base_fee_adult : age_group == 'child' ? $base_fee_child : $base_fee_senior
+
+	discount_type = decide_fee_type()
+	if discount_type == 'holiday' then
+		return base_fee + increase_amount_holiday
+	elsif discount_type == 'mon_wed' then
+		return base_fee - discount_amount_mon_wed
+	elsif discount_type == 'night' then
+		return base_fee - discount_amount_night
+	else
+		return base_fee
+	end
+end
+
+GROUP_DISCONT_RATE = 0.9
+
+fee_adult = decide_fee('adult')
+fee_adult_sp = 600
+DISCOUNT_ADULT_DECIDED = fee_adult - fee_adult_sp
+fee_child = decide_fee('child')
+fee_child_sp = 400
+DISCOUNT_CHILD_DECIDED = fee_child - fee_child_sp
+fee_senior = decide_fee('senior')
+fee_senior_sp = 500
+DISCOUNT_SENIOR_DECIDED = fee_senior - fee_senior_sp
+
+total_fee = 0
+raw_fee = 0
+details = ''
+
+process_end = false
 
 clear_console()
 
@@ -116,34 +162,44 @@ while !process_end
 
 	clear_console()
 
+	total_person = adult_normal.to_i + child_normal.to_i + senior_normal.to_i
 	# 団体割引 → 10人以上で10%割引（子供は 0.5 人換算とする）
-	total_person = adult_normal.to_i + child_normal.to_i / 2 + senior_normal.to_i
-	is_group_discount = total_person >= 10
+	total_person_for_discount = adult_normal.to_i + child_normal.to_i / 2 + senior_normal.to_i
+	is_group_discount = total_person_for_discount >= 10
 
-	# TODO: 夕方料金 → 17時以降は 100円 引き
-	#   - 現在時刻を取得して変数nowに代入 → now = Time.now
-	# TODO: 休日料金 → 土日祝は 200円 増し
-	# TODO: 月水割引 → 月曜と水曜は 100円 引き
+	raw_base_fee_adult = adult_normal.to_i * $base_fee_adult
+	raw_base_fee_child = child_normal.to_i * $base_fee_child
+	raw_base_fee_senior = senior_normal.to_i * $base_fee_senior
 	raw_fee_adult = adult_normal.to_i * fee_adult
 	raw_fee_child = child_normal.to_i * fee_child
 	raw_fee_senior = senior_normal.to_i * fee_senior
-	discount_adult_total = adult_special.to_i * DISCOUNT_ADULT
-	discount_child_total = child_special.to_i * DISCOUNT_CHILD
-	discount_senior_total = senior_special.to_i * DISCOUNT_SENIOR
+	discount_adult_total = adult_special.to_i * DISCOUNT_ADULT_DECIDED
+	discount_child_total = child_special.to_i * DISCOUNT_CHILD_DECIDED
+	discount_senior_total = senior_special.to_i * DISCOUNT_SENIOR_DECIDED
 	# TODO: 全てのチケット料金を計算して、変数total_feeに代入
 	total_fee =
 		raw_fee_adult - discount_adult_total +
 		raw_fee_child - discount_child_total +
 		raw_fee_senior - discount_senior_total
-	if is_group_discount then
-		total_fee *= GROUP_DISCONT_RATE
-	end
-	raw_fee = total_fee
+	# raw_fee = total_fee
+	raw_fee = raw_base_fee_adult + raw_base_fee_child + raw_base_fee_senior
 
-	details = "|        |   通常   |   割引   |\n+--------+----------+----------+\n|  大人  |  #{adult_normal} 名様 |  #{adult_special} 名様 |\n|  子供  |  #{child_normal} 名様 |  #{child_special} 名様 |\n| シニア |  #{senior_normal} 名様 |  #{senior_special} 名様 |\n+--------+----------+----------+"
+	# 割引の計算
+	if !is_night() then
+		if is_group_discount then
+			total_fee *= GROUP_DISCONT_RATE
+		end
+	end
+
+	details = "|        |   通常   |   割引   |   割増   |\n+--------+----------+----------+\n|  大人  |  #{adult_normal} 名様 |  ￥#{discount_adult_total}  |\n|  子供  |  #{child_normal} 名様 |  ￥#{discount_child_total}  |\n| シニア |  #{senior_normal} 名様 |  ￥#{discount_senior_total}  |\n+--------+----------+----------+"
 	# puts "大人:#{adult_normal}\n子供:#{child_normal}\nシニア:#{senior_normal}\n大人（特別）:#{adult_special}\n子供（特別）:#{child_special}\nシニア（特別）:#{senior_special}"
 	# TODO: impl
-	puts "販売合計金額:#{total_fee}\n金額変更前合計金額:#{raw_fee}\n金額変更明細:\n#{details}\n"
+	puts "合計人数:#{total_person} 名"
+	puts "団体割引:#{is_group_discount}"
+	puts "その他割引・割増:#{decide_fee_type()}"
+	puts "販売合計金額:#{total_fee}"
+	puts "金額変更前合計金額:#{raw_fee}"
+	puts "金額変更明細:\n#{details}\n"
 
 	while true
 		print "処理を継続しますか？(yes | no)> "
