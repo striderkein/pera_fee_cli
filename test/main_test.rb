74653e322e2810f12457e124d942aef890f72a78
shirow ozawa <striderkein@users.noreply.github.com>
2023-08-21 12:10:57 +0900

    test:「平日昼間料金」のテストを追加

--- test/main_test.rb
+++ test/main_test.rb
@@ -18,4 +18,25 @@ class MainTest < Minitest::Test
   def test_is_number_with_empty
     assert_equal false, is_number('')
   end
+
+  def test_decide_fee
+    ###################
+    # 割引なしパターン
+    ###################
+    # 平日
+    Time.stub(:now, Time.new(2023, 8, 22, 10, 0, 0)) do
+      # 大人（通常）
+      assert_equal 1000, decide_fee('adult', false)
+      # 子供（通常）
+      assert_equal 500, decide_fee('child', false)
+      # シニア（通常）
+      assert_equal 800, decide_fee('senior', false)
+      # 大人（特別）
+      assert_equal 600, decide_fee('adult', true)
+      # 子供（特別）
+      assert_equal 400, decide_fee('child', true)
+      # シニア（特別）
+      assert_equal 500, decide_fee('senior', true)
+    end
+  end
 end
