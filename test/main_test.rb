da76e78b26d29a44677cd4365108aa86ca6714ab
shirow ozawa <striderkein@users.noreply.github.com>
2023-08-21 12:16:39 +0900

    test:「月水割（仮）」のテストを追加

--- test/main_test.rb
+++ test/main_test.rb
@@ -57,5 +57,21 @@ class MainTest < Minitest::Test
       # シニア（特別）
       assert_equal 400, decide_fee('senior', true)
     end
+
+    # 月水割（仮）
+    Time.stub(:now, Time.new(2023, 8, 23, 10, 0, 0)) do
+      # 大人（通常）
+      assert_equal 900, decide_fee('adult', false)
+      # 子供（通常）
+      assert_equal 400, decide_fee('child', false)
+      # シニア（通常）
+      assert_equal 700, decide_fee('senior', false)
+      # 大人（特別）
+      assert_equal 500, decide_fee('adult', true)
+      # 子供（特別）
+      assert_equal 300, decide_fee('child', true)
+      # シニア（特別）
+      assert_equal 400, decide_fee('senior', true)
+    end
   end
 end
